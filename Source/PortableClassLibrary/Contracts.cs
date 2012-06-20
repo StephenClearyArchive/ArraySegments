6cbf6fe97a602a325d23eaabffdbab856e11a3f8
Stephen Cleary <StephenCleary@users.noreply.github.com>
2012-06-20 12:17:38 -0400

    Merge Main to Release.

---
+++ Source/PortableClassLibrary/Contracts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// All changes to this file should be pushed to the PortableClassLibraryFakeContracts solution.
+
+namespace System.Diagnostics.Contracts
+{
+    internal static class Contract
+    {
+        public static T Result<T>() { return default(T); }
+        public static T OldValue<T>(T value) { return value; }
+
+        [Conditional("never")]
+        public static void Ensures(bool condition) { }
+
+        [Conditional("never")]
+        public static void Requires(bool condition) { }
+
+        [Conditional("never")]
+        public static void Assume(bool condition) { }
+
+        [Conditional("never")]
+        public static void Assert(bool condition) { }
+
+        [Conditional("never")]
+        public static void Invariant(bool condition) { }
+    }
+
+    internal sealed class ContractInvariantMethodAttribute : Attribute
+    {
+    }
+}
---
+++ Source/PortableClassLibrary/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyDescription("Lightweight extension methods for ArraySegment, particularly useful for byte arrays.")]
+[assembly: AssemblyCompany("Nito Programs")]
+[assembly: AssemblyCopyright("Copyright © 2012 Nito Programs")]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.0.1")]
