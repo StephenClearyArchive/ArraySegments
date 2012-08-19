f21b753d095132cedad6d36ec8b8bc3a326c3146
stephen <stephen@Bob>
2012-08-19 07:29:10 -0400

    Added one unit test, just to ensure that the new ArraySegment IEnumerable support wouldn't break our extension methods.

---
+++ Source/Unit Tests/ArraySegmentExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArraySegments;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class ArraySegmentExtensions
+    {
+        [TestMethod]
+        public void Take_TakesElements()
+        {
+            var segment = new[] { 1, 2, 3 }.AsArraySegment();
+            ArraySegment<int> result = segment.Take(1);
+            Assert.AreSame(segment.Array, result.Array);
+            Assert.AreEqual(segment.Offset, result.Offset);
+            Assert.AreEqual(result.Count, 1);
+            Assert.IsTrue(result.SequenceEqual(new[] { 1 }));
+        }
+    }
+}
