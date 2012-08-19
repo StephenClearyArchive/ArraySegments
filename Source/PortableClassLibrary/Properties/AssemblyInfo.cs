036ed5590b81d65290730802550de19cef717e8d
stephen <stephen@Bob>
2012-08-19 07:20:30 -0400

    Added .NET 4.5 and Windows Store for code contracts and IReadOnlyList.

--- Source/PortableClassLibrary/Properties/AssemblyInfo.cs
+++ Source/PortableClassLibrary/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("ArraySegment extension methods for .NET 4.0 and higher, Metro, Silverlight 4 and higher, Windows Phone 7 and higher, and XBox 360")]
+[assembly: AssemblyTitle("ArraySegment extension methods for .NET 4.0 and higher, Windows Store, Silverlight 4 and higher, Windows Phone 7 and higher, and XBox 360")]
